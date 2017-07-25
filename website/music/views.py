ca6f32151d8fa055d6b398e635e32f171e5c2c89
Наталя Піманкова <natalya2487@gmail.com>
2017-07-25 10:11:24 +0300

    list of albums in view

--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>This will be a list of all albums</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 
 def detail(request, album_id):
