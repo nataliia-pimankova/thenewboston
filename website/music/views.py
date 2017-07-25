ab0d67e6b1c28859123d4193cf0d944b41e643a6
Наталя Піманкова <natalya2487@gmail.com>
2017-07-25 11:31:10 +0300

    add app_name, get_object_or_http404

--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Album
 
@@ -12,9 +12,6 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html',
                   {'album': album})
