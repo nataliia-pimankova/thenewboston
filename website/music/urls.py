cb479a68195d931145ca50111cd97ab1003fcf7e
Наталя Піманкова <natalya2487@gmail.com>
2017-07-26 12:07:32 +0300

    add createView, UpdateView, DeleteView add bootstrap styles

--- website/music/urls.py
+++ website/music/urls.py
@@ -10,7 +10,13 @@ urlpatterns = [
     # /music/71/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/71/favorite/
-    # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
